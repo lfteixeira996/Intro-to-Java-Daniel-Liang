cf3b72ef5c9bd795ccd2f766efd21a51cf128e60
Luis Teixeira <filipe.teixeira.996@gmail.com>
2017-06-26 15:28:44 +0200

    Files updated

--- workspace/Chapter_03/src/Exercise_03_18.java
+++ workspace/Chapter_03/src/Exercise_03_18.java
@@ -1,46 +1,53 @@
 import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Exercise_03_18 {
 
 	public static void main(String[] args) throws IOException
 	{	
-		double w, cost;
-		
 		Scanner input  = new Scanner(System.in);
-	
+		double w;
+		double C = 0;
+		
 		System.out.print("Enter the weight of the package: ");
 		w = input.nextDouble();
 		
-		
-		if((w > 0) && (w <= 1))
+		if((w>0) || (w<=1))
 		{
-			cost = 3.5;
-			System.out.print("The cost of the package is: " + cost);
+			C = 3.5;
+			System.out.println("The cost is " +C+ " dollars");
 		}
 		
-		else if((w > 1) && (w <= 3))
+		else if((w>1) || (w<=3))
 		{
-			cost = 5.5;
-			System.out.print("The cost of the package is: " + cost);
+			C = 5.5;
+			System.out.println("The cost is " +C+ " dollars");
 		}
 		
-		else if((w > 3) && (w <= 10))
+		else if((w>3) || (w<=10))
 		{
-			cost = 8.5;
-			System.out.print("The cost of the package is: " + cost);
+			C = 8.5;
+			System.out.println("The cost is " +C+ " dollars");
 		}
 		
-		else if((w > 10) && (w <= 20))
+		else if((w>10) || (w<=20))
 		{
-			cost = 10.5;
-			System.out.print("The cost of the package is: " + cost);
+			C = 10.5;
+			System.out.println("The cost is " +C+ " dollars");
 		}
 		
-		else if( w > 50)
+		
+		else if(w>50)
 		{
-			System.out.print("The package cannot be shipped");
+			System.out.println("The package cannot be shipped");
+	
 		}
+		
+		
+		
+		
 	}
 
 }
